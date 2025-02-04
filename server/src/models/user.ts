42217eecb84ba4c89589b44c6805a61d464d06f8
Hydrangea <dearlove.matthew@gmail.com>
2025-02-03 22:58:17 -0500

    Created the user model for login functionality

---
+++ server/src/models/user.ts
@@ -0,0 +1,55 @@
+import { DataTypes, Sequelize, Model, Optional } from 'sequelize';
+import bcrypt from 'bcrypt';
+
+// interface to define the components of a user.
+interface UserAttributes {
+    id: number;
+    username: string;
+    password: string;
+}
+
+// sets id as optional
+interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
+
+export class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
+    public id!: number;
+    public username!: string;
+    public password!: string;
+
+    // hashes the password prior to database storage for security.
+    public async hashPassword(password: string) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(password, saltRounds);
+    }
+}
+
+export function UserGenerator(sequelize: Sequelize): typeof User {
+    User.init(
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            tableName: 'profiles',
+            sequelize,
+            hooks: {
+                beforeCreate: async (user: User) => {
+                    await user.hashPassword(user.password);
+                },
+            }
+        }
+    );
+
+    return User;
+}
