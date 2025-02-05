c6b7acfa59036022f7ae18bacfd514f00ceb7e15
Hydrangea <dearlove.matthew@gmail.com>
2025-02-04 20:29:27 -0500

    Further login functionality setup

---
+++ server/src/middleware/auth.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+interface JwtPayload {
+  username: string;
+}
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided.'});
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY || 'defaultsecret') as JwtPayload;
+    
+    req.body.username = decoded.username;
+    next();
+  } catch (error) {
+    return res.status(403).json({ message: 'Invalid token'})
+  }
+};
---
+++ server/src/routes/api/user-routes.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { getUserById, createUser } from '../../controllers/user-controller.js';
+
+const router = express.Router();
+
+// GET request - gets user by id
+router.get('/:id', getUserById);
+
+// POST request - creates a new user
+router.post('/', createUser);
+
+export { router as userRouter };
