05bbc573f5eff48ef146802d7230cbeb7b1d0c98
Danielle Woessner <dlegare7792@gmail.com>
2025-02-06 23:08:21 -0500

    Update Mainpage.tsx
    Commit Changes to Danielle Branch

--- client/src/pages/Mainpage.tsx
+++ client/src/pages/Mainpage.tsx
@@ -1,11 +1,25 @@
-//import { useState, useEffect, MouseEventHandler } from "react";
-//import { Link } from "react-router-dom";
+import React from "react";
 
-const MainPage = () => {
+const MainPage: React.FC = () => {
   return (
-    <div>
-      <p>Main page</p>
-    </div>
+   <>
+   <head>
+      <img
+        src="/Spoonfed.png"
+        alt="Spoonfed"
+        className="relative w-20 h-20 md:w-24 md:h-24 rounded-full object-cover shadow-xl border-4 border-white mx-auto" />
+    </head>
+    <div className="container mx-auto px-4 py-8">
+        <div className="flex justify-center mb-8 px-4"></div>
+        <h1 className="text-4xl font-bold mb-6">Welcome to Spoon Fed</h1>
+        <p className="text-xl text-gray-600">
+          Your personal recipe discovery and management platform
+        </p>
+        <img
+          src="/Spoonfed.png"
+          alt="Spoonfed"
+          className="relative w-20 h-20 md:w-24 md:h-24 rounded-full object-cover shadow-xl border-4 border-white mx-auto" />
+      </div></>
   );
 };
 
