dcbba9397e69348cba0df92aaa084847dd78434a
Daniellewoessner <dlegare7792@gmail.com>
2025-02-11 21:38:52 -0500

    added render deployment settings

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -10,19 +10,19 @@ const sequelize_options: Options = {
   dialect: "postgres",
   dialectOptions: {
     decimalNumbers: true,
-    /*
+    
     ssl: {
       require: true,
       rejectUnauthorized: false, // Important for self-signed certificates
-    },*/
+    },
   },
 };
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL, sequelize_options)
+  ? new Sequelize(process.env.DB_URL as string, sequelize_options)
   : new Sequelize(
       process.env.DB_NAME || "",
-      process.env.DB_USER || "",
+      process.env.LOCAL_DB_USER || "",
       process.env.DB_PASSWORD,
       sequelize_options
     );
