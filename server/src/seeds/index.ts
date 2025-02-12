13fc956b2ba5d79ec3a7027f49febccb5b87201a
Daniellewoessner <dlegare7792@gmail.com>
2025-02-11 20:37:02 -0500

    git commit origin main

---
+++ server/src/seeds/index.ts
@@ -0,0 +1,21 @@
+import { seedUsers } from "./user-seeds.js";
+import { sequelize } from '../models/index.js';
+
+const seed = async (): Promise<void> => {
+    try {
+        await sequelize.sync({ force: true });
+        await seedUsers();
+
+        process.exit(0); 
+    } catch (err)   {
+        console.error('error seeding datbase');
+        process.exit(1);
+        
+
+        
+    }
+};
+
+seed();
+
+
